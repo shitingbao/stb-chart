2f5386e716008c889f40f6bef17db53331e2bf4d
shitingbao <871258317@qq.com>
2019-11-05 17:49:16 +0800

    element

--- stb-chart/src/main.js
+++ stb-chart/src/main.js
@@ -6,12 +6,18 @@ import axios from 'axios';
 import { con } from './content';
 import echarts from 'echarts';
 import { sendSock } from './socket/socket';
+// import { Button, Select } from 'element-ui';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.prototype.$echarts = echarts;
 Vue.prototype.$conMsg = con;
 Vue.prototype.$wsSend = sendSock;
 Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
+// Vue.use(Button);
+// Vue.use(Select);
 
 new Vue({
 	router,
