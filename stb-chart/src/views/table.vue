bd5c78f92eaf2a31abf186e7abff69abdf6477e4
shitingbao <871258317@qq.com>
2019-11-06 14:45:03 +0800

    select

--- stb-chart/src/views/table.vue
+++ stb-chart/src/views/table.vue
@@ -1,16 +1,42 @@
 <template>
   <div class="bg">
     <button @click="getMsg()">getclicktest</button>
+    <div class="select">
+      <el-select v-model="columnvalue" clearable placeholder="选择筛选列" @change="selectData">
+        <el-option
+          v-for="item in columnoptions"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        ></el-option>
+      </el-select>
+
+      <el-select v-model="conditionvalue" clearable placeholder="选择条件列" @change="selectData">
+        <el-option
+          v-for="item in conditionoptions"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        ></el-option>
+      </el-select>
+
+      <el-input
+        class="select-input"
+        placeholder="Please input your condition value"
+        v-model="inputData"
+      ></el-input>
+    </div>
+
     <div class="tb">
       <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="dis" label="dis" width="160"></el-table-column>
-        <el-table-column prop="alias" label="alias" width="55"></el-table-column>
+        <el-table-column prop="dis" label="dis"></el-table-column>
+        <el-table-column prop="alias" label="alias"></el-table-column>
         <el-table-column prop="company" label="company"></el-table-column>
-        <el-table-column prop="department" label="department" width="180"></el-table-column>
-        <el-table-column prop="crd_no" label="crd_no" width="120"></el-table-column>
+        <el-table-column prop="department" label="department"></el-table-column>
+        <el-table-column prop="crd_no" label="crd_no"></el-table-column>
         <el-table-column prop="truck_no" label="truck_no"></el-table-column>
-        <el-table-column prop="truck_type" label="truck_type" width="180"></el-table-column>
-        <el-table-column prop="last_conn" label="last_conn" width="180"></el-table-column>
+        <el-table-column prop="truck_type" label="truck_type"></el-table-column>
+        <el-table-column prop="last_conn" label="last_conn"></el-table-column>
         <el-table-column prop="online" label="online"></el-table-column>
       </el-table>
     </div>
@@ -21,11 +47,74 @@
 export default {
   data() {
     return {
+      inputData: "",
       test: null,
-      tableData: []
+      tableData: [],
+      showTableData: [],
+      columnvalue: "",
+      conditionvalue: "",
+      columnoptions: [
+        {
+          value: "dis",
+          label: "dis"
+        },
+        {
+          value: "alias",
+          label: "alias"
+        },
+        {
+          value: "company",
+          label: "company"
+        },
+        {
+          value: "department",
+          label: "department"
+        },
+        {
+          value: "crd_no",
+          label: "crd_no"
+        },
+        {
+          value: "truck_no",
+          label: "truck_no"
+        },
+        {
+          value: "truck_type",
+          label: "truck_type"
+        },
+        {
+          value: "last_conn",
+          label: "last_conn"
+        },
+        {
+          value: "online",
+          label: "online"
+        }
+      ],
+      conditionoptions: [
+        {
+          value: "=",
+          label: "等于"
+        },
+        {
+          value: ">",
+          label: "大于"
+        },
+        {
+          value: "<",
+          label: "小于"
+        },
+        {
+          value: "!=",
+          label: "不等于"
+        }
+      ]
     };
   },
   methods: {
+    selectData: function(value) {
+      console.log("your select option is:", value);
+    },
     getMsg: function() {
       console.log("start get");
       let that = this;
@@ -42,6 +131,7 @@ export default {
           console.log("data:", e);
           that.test = e.data;
           that.tableData = e.data;
+          that.showTableData = e.data;
         })
         .catch(function(err) {
           console.log("this error is:", err);
@@ -56,10 +146,18 @@ export default {
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  width: 100%;
   .tb {
+    width: 100%;
     border: 1px solid rgb(160, 157, 157);
     .tb-column {
       width: 50px;
+    }
+  }
+  .select {
+    flex-direction: row;
+    .select-input {
+      width: 200px;
     }
   }
 }
