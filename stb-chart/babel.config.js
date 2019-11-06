c3d6aa5444bdb69f01f893277bc156d81dd2ebef
shitingbao <871258317@qq.com>
2019-11-06 09:49:02 +0800

    Revert "element"
    This reverts commit 2f5386e716008c889f40f6bef17db53331e2bf4d.

--- stb-chart/babel.config.js
+++ stb-chart/babel.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-	presets: [['es2015', { modules: false }]],
-	plugins: [
-		[
-			'component',
-			{
-				libraryName: 'element-ui',
-				styleLibraryName: 'theme-chalk'
-			}
-		]
-	]
+  presets: ["@vue/cli-plugin-babel/preset"]
 };
