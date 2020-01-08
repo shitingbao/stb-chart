fa21da48d318fa6abb6191402e7219f96edb2a84
shitingbao <871258317@qq.com>
2020-01-08 16:11:09 +0800

    yarn

--- stb-chart/src/components/Upload.vue
+++ stb-chart/src/components/Upload.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <el-upload class="upload-demo" ref="upload" action="http://localhost:8088/formfile" :on-preview="handlePreview" :on-remove="handleRemove" :file-list="fileList" :auto-upload="false" :on-success="handsuccess">
+    <el-upload
+      class="upload-demo"
+      ref="upload"
+      action="http://localhost:8088/formfile"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :file-list="fileList"
+      :auto-upload="false"
+      :on-success="handsuccess"
+    >
       <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
       <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
       <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
@@ -13,18 +22,7 @@
 export default {
   data() {
     return {
-      fileList: [
-        {
-          name: "food.jpeg",
-          url:
-            "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100"
-        },
-        {
-          name: "food2.jpeg",
-          url:
-            "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100"
-        }
-      ]
+      fileList: []
     };
   },
   methods: {
