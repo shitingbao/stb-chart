4d0f802072e7eb9866f449303443c92c8908bf7f
shitingbao <871258317@qq.com>
2020-03-30 13:41:17 +0800

    默认时间选择器

--- stb-chart/src/components/HelloWorld.vue
+++ stb-chart/src/components/HelloWorld.vue
@@ -1,11 +1,54 @@
 <template>
   <div class="HelloWorld">
     <h1>{{ msg }}</h1>
+    <el-date-picker
+      v-model="value1"
+      type="daterange"
+      range-separator="至"
+      start-placeholder="开始日期"
+      end-placeholder="结束日期"
+    ></el-date-picker>
+    <button @click="show()">默认</button>
   </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    show() {
+      console.log("date:", this.value1);
+      this.value1.push(this.getDay(-7));
+      this.value1.push(new Date());
+    },
+    // 获取当前时间，day为number，getDay(-1):昨天的日期;getDay(0):今天的日期;getDay(1):明天的日期;【以此类推】
+    getDay(day) {
+      var today = new Date();
+      var targetday_milliseconds = today.getTime() + 1000 * 60 * 60 * 24 * day;
+      today.setTime(targetday_milliseconds); //注意，这行是关键代码
+      var tYear = today.getFullYear();
+      var tMonth = today.getMonth();
+      var tDate = today.getDate();
+      tMonth = this.doHandleMonth(tMonth + 1);
+      tDate = this.doHandleMonth(tDate);
+      return tYear + "-" + tMonth + "-" + tDate;
+    },
+    doHandleMonth(month) {
+      var m = month;
+      if (month.toString().length == 1) {
+        m = "0" + month;
+      }
+      return m;
+    }
+  },
+  mounted() {
+    this.value1.push(this.getDay(-7));
+    this.value1.push(new Date());
+  },
+  data() {
+    return {
+      value1: []
+    };
+  },
   name: "HelloWorld",
   props: {
     msg: String
