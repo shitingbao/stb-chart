4e40d169320cb38cf9619a094377a62a1fa42b21
shitingbao <871258317@qq.com>
2020-04-03 13:22:30 +0800

    标题名称

--- stb-chart/src/views/Home.vue
+++ stb-chart/src/views/Home.vue
@@ -12,21 +12,21 @@
       <el-menu-item index="1">主页</el-menu-item>
       <el-submenu index="2">
         <template slot="title">功能选择项</template>
-        <el-menu-item index="about">about</el-menu-item>
-        <el-menu-item index="chart">chart</el-menu-item>
-        <el-menu-item index="chatroom">chatroom</el-menu-item>
-        <el-menu-item index="imageupload">imageupload</el-menu-item>
-        <el-menu-item index="table">table</el-menu-item>
+        <el-menu-item index="about">待定</el-menu-item>
+        <el-menu-item index="chart">待定</el-menu-item>
+        <el-menu-item index="chatroom">聊天室</el-menu-item>
+        <el-menu-item index="imageupload">图片文字转换</el-menu-item>
+        <el-menu-item index="table">待定</el-menu-item>
         <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项1</el-menu-item>
-          <el-menu-item index="2-4-2">选项2</el-menu-item>
-          <el-menu-item index="2-4-3">选项3</el-menu-item>
+          <template slot="title">待定0</template>
+          <el-menu-item index="2-4-1">待定1</el-menu-item>
+          <el-menu-item index="2-4-2">待定2</el-menu-item>
+          <el-menu-item index="2-4-3">待定3</el-menu-item>
         </el-submenu>
       </el-submenu>
       <el-menu-item index="3" disabled>消息中心</el-menu-item>
       <el-menu-item index="4">
-        <span target="_blank">用户管理</span>
+        <span>用户管理</span>
       </el-menu-item>
     </el-menu>
     <div>
