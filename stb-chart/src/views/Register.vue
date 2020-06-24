b5e303fbc36f756be88944330ce94a5dacade8de
shitingbao <871258317@qq.com>
2020-06-24 16:40:02 +0800

    register html

--- stb-chart/src/views/Register.vue
+++ stb-chart/src/views/Register.vue
@@ -1,7 +1,23 @@
 <template>
-  <div v-if="isLogin">
-    <el-input placeholder="请输入用户名" v-model="username" clearable></el-input>
-    <el-input placeholder="请输入密码" v-model="pwd" show-password></el-input>
+  <div class="registered">
+    <div class="registered-title">Join StbWeb</div>
+    <h1 class="registered-word">
+      Create your account!
+      <div class="registered-word-title">
+        or
+        <a @click="returnLogin">sign in</a>
+      </div>
+    </h1>
+    <div class="box-width registered-label">
+      Username
+      <div class="star">*</div>
+    </div>
+    <el-input class="box-width input" placeholder="your username" v-model="username" clearable></el-input>
+    <div class="box-width registered-label">
+      Password
+      <div class="star">*</div>
+    </div>
+    <el-input class="box-width input" placeholder="your password" v-model="pwd" show-password></el-input>
     <el-button type="primary" @click="register">注册</el-button>
   </div>
 </template>
@@ -9,6 +25,9 @@
 <script>
 export default {
   methods: {
+    returnLogin() {
+      this.$router.push({ name: "login" });
+    },
     register() {
       let config = {
         headers: { "stbweb-api": "register" }
@@ -51,4 +70,49 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.box-width {
+  width: 540px;
+}
+.star {
+  padding-left: 5px;
+  color: #cb2431;
+  content: "*";
+}
+.registered {
+  display: flex;
+  padding: 0px;
+  margin: 0px;
+  width: 100%;
+  height: 100%;
+  // background: yellow;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .registered-word {
+    display: flex;
+    flex-direction: row;
+    .registered-word-title {
+      margin-top: 5px;
+      font-size: 24px;
+      a {
+        color: #0366d6;
+        text-decoration: none;
+        font-size: 15px;
+        cursor: pointer;
+      }
+    }
+  }
+  .registered-title {
+    padding-top: 50px;
+  }
+  .registered-label {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 7px;
+  }
+  .input {
+    padding-bottom: 30px;
+    height: 25px;
+  }
+}
 </style>
