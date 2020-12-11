2269bf1d6ed2e3975365604970ceae5a5532f84a
shitingbao <871258317@qq.com>
2020-12-11 17:38:18 +0800

    聊天页面改进

--- stb-chart/src/views/business/Commodity.vue
+++ stb-chart/src/views/business/Commodity.vue
@@ -1,28 +1,66 @@
 <template>
   <div class="business">
-    <div class="block">
-      <span class="demonstration">轮播图</span>
-      <el-carousel trigger="click" height="150px" class="carousel">
-        <el-carousel-item v-for="item in 4" :key="item">
-          <h3 class="small">{{ item }}</h3>
-        </el-carousel-item>
-      </el-carousel>
+    <el-carousel trigger="click" height="150px" class="carousel">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="small">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+
+    <div class="base-title">
+      <span>功能</span>
+      <span>选择区</span>
     </div>
-    <div class="select-base">
-      <div class="base-title">
-        <span>基础</span>
-        <span>第一类</span>
+    <div class="base-list">
+      <div @click="chat">
+        <span>在线交友</span><br />
+        <span>给你一个展示自我的平台</span><br />
+        <span>认识更多志同道合的伙伴</span>
       </div>
-      <div class="base-list">
-        <div @click="chat">在线交友</div>
-        <div @click="commoditylist">商品优选</div>
-        <div>学习教材</div>
-        <div>关于我们</div>
+      <div @click="commoditylist">
+        <span>商品优选</span><br />
+        <span>展示一些个人喜好的物品</span><br />
+        <span>大多是依附主题的物品</span>
+      </div>
+      <div>
+        <span>学习教材</span><br />
+        <span>主要是该平台的发展史</span><br />
+        <span>相同爱好的马上加入我们吧</span>
+      </div>
+      <div>
+        <span>关于我们</span><br />
+        <span>猥琐发育小团体</span>
+      </div>
+    </div>
+
+    <div class="base-title">
+      <span>办公</span>
+      <span>辅助区</span>
+    </div>
+    <div class="base-list">
+      <div @click="chat">
+        <span>提取图片中的文字</span><br />
+        <span>1.准备要获取文字的图片</span><br />
+        <span>2.上传指定区域</span><br />
+        <span>3.获取你想要的内容</span>
+      </div>
+      <div @click="commoditylist">
+        <span>文件内容比较</span><br />
+        <span>比较两个文件的不同</span><br />
+        <span>只支持txt，以及csv文件</span>
+      </div>
+      <div>
+        <span>excel转csv或者txt</span><br />
+        <span>将excel文件转为其他文件</span><br />
+        <span>可指定一部分转化条件</span>
+      </div>
+      <div>
+        <span>后期功能</span><br />
+        <span>待定</span>
       </div>
     </div>
     <div class="select-expand">
       <div class="expand-title">
-        <span>介绍</span>
+        <span>Give me a cup of coffee</span>
       </div>
     </div>
   </div>
@@ -61,6 +99,7 @@ export default {
   .carousel {
     width: 1200px;
     justify-content: center;
+    padding: 20px 0;
   }
   .base-list {
     display: flex;
--- stb-chart/src/views/chat/Chathome.vue
+++ stb-chart/src/views/chat/Chathome.vue
@@ -1,36 +1,38 @@
 <template>
-  <div class="chat">
-    <div class="chat-title">
-      <div>select</div>
-      <div>create room</div>
-      <div>host infomations</div>
+  <div class="flex-container">
+    <div class="flex-item">
+      <div style="height:80px;line-height:100px;" class="chat-title">
+        <div>刷新</div>
+        <div>create room</div>
+        <div>host infomations</div>
+        <div>添加标签</div>
+        <div>确定搜索</div>
+      </div>
     </div>
-    <div class="chat-import">
-      <div class="chat-left">
-        <div class="chat-body">
-          <div
-            v-for="(item, index) in homelist"
-            :key="index"
-            @click="intoRoom"
-            class="chat-body-room"
-          >
-            <span
-              >homeID:{{ item.homeID }} homeName:{{ item.homeName }} num:{{
-                item.num
-              }}
-              /{{ item.maxNum }}</span
+    <div class="flex-item">
+      <div style="color:#fff;" class="chat">
+        <div class="chat-left">
+          <div class="chat-body">
+            <div
+              v-for="(item, index) in homelist"
+              :key="index"
+              @click="intoRoom"
+              class="chat-body-room"
             >
-
-            <span>homeCommon:{{ item.homeCommon }}</span>
+              <span>房间名称:{{ item.homeName }}</span>
+              <span>人数:{{ item.num }} /{{ item.maxNum }}</span>
+              <span>房间类型:{{ item.homeType }}</span>
+              <span>简介:{{ item.homeCommon }}</span>
+            </div>
           </div>
         </div>
-        <div class="block">
-          <span class="demonstration">大于 7 页时的效果</span>
-          <el-pagination layout="prev, pager, next" :total="1000">
-          </el-pagination>
-        </div>
+        <div class="chat-right">世界窗口</div>
       </div>
-      <div class="chat-right">世界窗口</div>
+    </div>
+    <div class="flex-item">
+      <div style="height:120px;line-height:100px;">
+        <button>换一批</button>
+      </div>
     </div>
   </div>
 </template>
@@ -41,37 +43,74 @@ export default {
     return {
       homelist: [
         {
-          homeID: "uxtd",
-          homeName: "mantou",
-          homeCommon: "my mantou",
+          homeID: "test1",
+          homeName: "test1",
+          homeCommon: "test1",
+          homeType: "test1",
           num: 5,
           maxNum: 8
         },
         {
-          homeID: "uxtd",
-          homeName: "mantou",
-          homeCommon: "my mantou",
+          homeID: "test1",
+          homeName: "test1",
+          homeCommon: "test1",
+          homeType: "test1",
           num: 5,
           maxNum: 8
         },
         {
-          homeID: "uxtd",
-          homeName: "mantou",
-          homeCommon: "my mantou",
+          homeID: "test1",
+          homeName: "test1",
+          homeCommon: "test1",
+          homeType: "test1",
           num: 5,
           maxNum: 8
         },
         {
-          homeID: "uxtd",
-          homeName: "mantou",
-          homeCommon: "my mantou",
+          homeID: "test1",
+          homeName: "test1",
+          homeCommon: "test1",
+          homeType: "test1",
           num: 5,
           maxNum: 8
         },
         {
-          homeID: "uxtd",
-          homeName: "mantou",
-          homeCommon: "my mantou",
+          homeID: "test1",
+          homeName: "test1",
+          homeCommon: "test1",
+          homeType: "test1",
+          num: 5,
+          maxNum: 8
+        },
+        {
+          homeID: "test1",
+          homeName: "test1",
+          homeCommon: "test1",
+          homeType: "test1",
+          num: 5,
+          maxNum: 8
+        },
+        {
+          homeID: "test1",
+          homeName: "test1",
+          homeCommon: "test1",
+          homeType: "test1",
+          num: 5,
+          maxNum: 8
+        },
+        {
+          homeID: "test1",
+          homeName: "test1",
+          homeCommon: "test1",
+          homeType: "test1",
+          num: 5,
+          maxNum: 8
+        },
+        {
+          homeID: "test1",
+          homeName: "test1",
+          homeCommon: "test1",
+          homeType: "test1",
           num: 5,
           maxNum: 8
         }
@@ -88,38 +127,79 @@ export default {
 };
 </script>
 <style lang="scss">
-.chat {
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.flex-container {
+  height: 100%;
   display: flex;
   flex-direction: column;
-  .chat-title {
-    display: flex;
-    flex-direction: row;
-    height: 60px;
-    div {
-      width: 150px;
-    }
+  text-align: center;
+}
+
+.flex-item:nth-child(1),
+.flex-item:nth-child(3) {
+  flex-grow: 0;
+  flex-shrink: 0;
+  background-color: #ababab;
+}
+
+.flex-item:nth-child(2) {
+  flex-grow: 1;
+  flex-shrink: 1;
+  background-color: #000;
+  overflow-y: auto;
+}
+
+.center {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  margin-top: -10px;
+  margin-left: -24px;
+}
+.chat-title {
+  display: flex;
+  flex-direction: row;
+
+  div {
+    width: 150px;
   }
-  .chat-import {
-    display: flex;
-    flex-direction: row;
+}
+.chat {
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  .chat-left {
+    width: 70%;
     .chat-body {
       display: flex;
       flex-wrap: wrap;
+      height: 90%;
+      margin-top: 30px;
+      margin-bottom: 20px;
       div {
-        width: 450px;
+        width: 320px;
         height: 150px;
+        padding-left: 10px;
       }
       .chat-body-room {
         display: flex;
         flex-direction: column;
-        margin: 10px;
+        // margin: 20px;
+        padding: 20px 20px 20px 20px;
         cursor: pointer;
       }
     }
-    .chat-right {
-      width: 500px;
-      border: 1px solid #d7dae2;
-    }
+  }
+  .chat-right {
+    width: 30%;
+    border: 1px solid #d7dae2;
+    margin-right: 20px;
   }
 }
 </style>
